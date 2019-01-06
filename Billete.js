114b6eac83adf21436a793ce6f654925aa09036c
sirpulpo <taz.vd@hotmail.com>
2019-01-06 15:44:11 -0600

    Añadiendo js de billetes

---
+++ Billete.js
@@ -0,0 +1,15 @@
+var imgBillete = {
+  100: "billete100.png",
+  50: "billete50.png",
+  20: "billete20.png",
+  10: "billete10.png",
+  5: "billete5.png"
+}
+class Billete {
+  constructor(v, c) {
+    this.valor = v;
+    this.cantidad = c;
+    this.img = new Image();
+    this.img.src =imgBillete[this.valor];
+  }
+}
